96a49a8590a14ec9458907ea9799cff3e0ec6154
nohoteth <loycecal@student.21-school.ru>
2022-08-18 16:10:00 +0300

    add DB open

--- src/prototype/db.go
+++ src/prototype/db.go
@@ -25,68 +25,29 @@ var dbInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=
 
 
 //Creating users table in database
-func createTableUsers() error {
+func openDatabase() *sql.DB {
 
 	//Connecting to database
 	db, err := sql.Open("postgres", dbInfo)
 	if err != nil {
-		return err
+		log.Panic(err)
 	}
-	defer db.Close()
-
+	
 	//Creating users Table
-	if _, err = db.Exec(`CREATE TABLE IF NOT EXISTS users(ID SERIAL PRIMARY KEY, LOGIN TEXT, USERNAME TEXT, ROLE INT, CAMPUS TEXT);`); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-//Creating events table in database
-func createTableEvents() error {
-
-	//Connecting to database
-	db, err := sql.Open("postgres", dbInfo)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users(ID SERIAL PRIMARY KEY, LOGIN TEXT, USERNAME TEXT, ROLE INT, CAMPUS TEXT);`)
 	if err != nil {
-		return err
+		log.Panic(err)
 	}
-	defer db.Close()
-
-	//Creating users Table
-	if _, err = db.Exec(`CREATE TABLE IF NOT EXISTS events(ID SERIAL PRIMARY KEY, TYPE TEXT, DESCRIPTION TEXT, UNIQUE_CODE TEXT, START_TIME TIMESTAMP, EXPIRIES_TIME TIMESTAMP);`); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-//Creating chats table in database
-func createTableChats() error {
-
-	//Connecting to database
-	db, err := sql.Open("postgres", dbInfo)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS events(ID SERIAL PRIMARY KEY, TYPE TEXT, DESCRIPTION TEXT, UNIQUE_CODE TEXT, START_TIME TIMESTAMP, EXPIRIES_TIME TIMESTAMP);`)
 	if err != nil {
-		return err
+		log.Panic(err)
 	}
-	defer db.Close()
-
-	//Creating chats Table
-	if _, err = db.Exec(`CREATE TABLE IF NOT EXISTS chats(ID SERIAL PRIMARY KEY, CHAT_ID BIGINT);`); err != nil {
-		return err
-	}
-
-	return nil
+	return db
 }
 
 //Add record in users table
-func addUser(us *user) error {
-	//Connecting to database
-	db, err := sql.Open("postgres", dbInfo)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
+func addUser(us *user, db *sql.DB) error {
+	
 	if _, err = db.Exec("INSERT INTO users (login, username, role, campus) values($1, $2, $3, $4)", us.login, us.name, 0, us.campus); err != nil {
 		return err
 	}
@@ -95,32 +56,15 @@ func addUser(us *user) error {
 }
 
 // Add new event in DB
-func addEvent(event *events) error {
-	//Connecting to database
-	db, err := sql.Open("postgres", dbInfo)
+func addEvent(event *events, db *sql.DB) error {
+	_, err = db.Exec("INSERT INTO event(TYPE, DESCRIPTION, UNIQUE_CODE, START_TIME, EXPIRIES_TIME) values('$1','$2','$3','$4','$5')")
 	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	if _, err = db.Exec("INSERT INTO event(TYPE, DESCRIPTION, UNIQUE_CODE, START_TIME, EXPIRIES_TIME) values('$1','$2','$3','$4','$5')"); err != nil {
-		return err
-	}
-
+        log.Panic(err)
 	return nil
 }
 
 //Check user in DB
-func checkUserExist(name string) (int, error) {
-
-	var count int
-	//Connecting to database
-	db, err := sql.Open("postgres", dbInfo)
-	if err != nil {
-		return 0, err
-	}
-	defer db.Close()
-
+func checkUserExist(name string, db *sql.DB) (int, error) {
 	//Counting number of users
 	row := db.QueryRow("SELECT * FROM users WHERE username = '$1'")
 	err = row.Scan(&count)
